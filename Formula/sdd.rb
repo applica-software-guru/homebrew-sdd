e034ba7ad850e2ff1971ac810bf809b694d7d146
Roberto Conte Rosito <roberto.conterosito@gmail.com>
2026-03-06 12:19:45 +0100

    Update sdd formula to 1.0.2

--- Formula/sdd.rb
+++ Formula/sdd.rb
@@ -1,8 +1,8 @@
 class Sdd < Formula
   desc "CLI for Story Driven Development"
   homepage "https://github.com/applica-software-guru/sdd"
-  url "https://registry.npmjs.org/@applica-software-guru/sdd/-/sdd-1.0.1.tgz"
-  sha256 "787e09a0ac1287b4a9aec2cc1c7378f454f837242daea168c17b6fd19acb573e"
+  url "https://registry.npmjs.org/@applica-software-guru/sdd/-/sdd-1.0.2.tgz"
+  sha256 "ed8c94ca61969ff87440069158a2fcfbc5f3bf61e9b1101ef34dce3fd59e1730"
   license "MIT"
 
   depends_on "node"
